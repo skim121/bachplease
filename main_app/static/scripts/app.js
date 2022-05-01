62bda8ca666b1768f333034ef2eba43293df75c9
skim121 <skim29121@gmail.com>
2022-05-01 10:40:16 -0500

    Home page styling in progress

--- main_app/static/scripts/app.js
+++ main_app/static/scripts/app.js
@@ -1,1 +1,18 @@
 console.log("sanity check");
+
+let $slider = document.getElementById('slider');
+let $toggle = document.getElementById('toggle');
+
+$toggle.addEventListener('click', function() {
+    let isOpen = $slider.classList.contains('slide-in');
+
+    $slider.setAttribute('class', isOpen ? 'slide-out' : 'slide-in');
+});
+
+let $slider2 = document.getElementById('slider2');
+
+$toggle.addEventListener('click', function() {
+    let isOpen = $slider2.classList.contains('slide-in2');
+
+    $slider2.setAttribute('class', isOpen ? 'slide-out' : 'slide-in2');
+});
